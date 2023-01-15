a5f7d81adc82260e1ee1418ae9a92fb97c896b2e
Abdelrahman Anany <60886012+AbdoAnany@users.noreply.github.com>
2023-01-15 12:56:39 +0200

    Create color.dart

---
+++ lib/src/color.dart
@@ -0,0 +1,46 @@
+import 'dart:ui';
+import 'package:flutter/widgets.dart';
+
+class AppColors{
+
+  static Color greyFont =const Color(0xff666666);
+  static Color grey =const Color(0xff999999);
+  static Color grey2 =const Color(0xff6c757d);
+  static Color grey1 =const Color(0xff798188);
+  static Color white1 =const Color(0xffffffff);
+  static Color white2 =const Color(0xffeeeeee);
+  static Color greyBlue = const Color(0xff0079ab);
+
+  static Color blue = const Color(0xff2196F3);
+  static Color glcRed = const Color(0xffe10000);
+
+  static Color glcOrange = const Color(0xfffd7e14);
+  static Color glcGreen = const Color(0xff72bd5a);
+
+
+  static List<Color> doneColor=[
+  const Color(0xff155724),
+  const Color(0xffd4edda),
+  const Color(0xffc3e6cb),
+  ];  static List<Color> warningColor=[
+  const Color(0xfffd7e14),
+  const Color(0xffedded4),
+  const Color(0xffe6d5c3),
+  ];
+  static List<Color> errorColor=[
+  const Color(0xffe20d0d),
+    const Color(0xfff8d7da),
+    const Color(0xfff5c6cb),
+  ];
+  static List<Color> infoColor=[
+    const Color(0xff0d97e2),
+    const Color(0xffd7ecf8),
+    const Color(0xffc6e0f5),
+  ];
+
+
+  static Color white =const Color(0xffffffff);
+  static Color black =const Color(0xff000000);
+  static Color transparent =const Color(0x00ffffff);
+
+}
